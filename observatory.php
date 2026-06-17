55b2a850003d1a0b70e32cf627299376434fc3a1
meerabharathi20-collab <meerabharathi20@gmail.com>
2026-06-17 17:08:57 +0530

    Add files via upload

--- observatory.php
+++ observatory.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>vellore travels - Tourist Places, Kavalur Observatory, Yelagiri, Amrithi Forest, hotels in vellore, vellore hotel, atm in vellore, vellore atm centre, banks in vellore, vellore banks, colleges, college in vellore</title>
-  <link rel="stylesheet" href="css/css.css?v=15">
-  <script src="js/js.js" defer></script>
+  <link rel="stylesheet" href="css.css?v=15">
+  <script src="js.js" defer></script>
   
 </head>
 <body id="tourist">
@@ -31,7 +31,7 @@
       <main class="content-area">
         <div class="contents">
           <h1><a href="tourist-places.php">Tourst Places</a> &raquo; <span>Kavalur Observatory</span></h1>
-<p>Kavalur observatory is located in Kavalur in the Javadu Hills in Alangayam, Vellore District. The Kavalur Observatory is located in a 100 acre forest land in Tamil Nadu, which is strewn with a variety of greenery of tropical region<img src="images/tourist-places/observatory.png" alt="Kavalur Observatory" style="float:right; margin:5px 0px 5px 10px;"  /> besides a number of medicinal plants with an occasional appearance of some wild life like deer, snakes and scorpions. Several varieties of birds have also been spotted in the campus. </p>
+<p>Kavalur observatory is located in Kavalur in the Javadu Hills in Alangayam, Vellore District. The Kavalur Observatory is located in a 100 acre forest land in Tamil Nadu, which is strewn with a variety of greenery of tropical region<img src="observatory.png" alt="Kavalur Observatory" style="float:right; margin:5px 0px 5px 10px;"  /> besides a number of medicinal plants with an occasional appearance of some wild life like deer, snakes and scorpions. Several varieties of birds have also been spotted in the campus. </p>
 <p>The observatory is at an altitude of 725m above mean sea level (longitude 78&deg; 49.6' E ; latitude 12&deg; 34.6' N). Apart from being reasonably away from city lights and industrial areas, the location has been chosen in order to be closer to the earth's equator for covering both northern and southern hemispheres with equal ease.</p>
 <p> In addition, its longitudinal position is such that it is the only major astronomical facility between Australia and South Africa for observing the southern objects.</p>
         </div>
